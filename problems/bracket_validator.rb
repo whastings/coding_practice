7322b7b91336d28dbeb49e99dcd24dcad28984e5
Will Hastings <will@objectdotcreate.net>
2014-08-23 13:07:01 -0700

    Solve bracket validator for hard case.

--- problems/bracket_validator.rb
+++ problems/bracket_validator.rb
@@ -1,23 +1,21 @@
-BRACKETS = [
-  ['{', '}'],
-  ['(', ')'],
-  ['[', ']']
-]
+BRACKETS = {
+  '{' => '}',
+  '(' => ')',
+  '[' => ']'
+}
+
+OPEN_PATTERN = /\A\{|\(|\[\z/
+CLOSE_PATTERN = /\A\}|\)|\]\z/
 
 def validate_brackets(string)
-  BRACKETS.each do |pair|
-    return false unless validate_bracket(*pair, string)
-  end
-  true
-end
-
-def validate_bracket(start_bracket, end_bracket, string)
   stack = []
   string.chars.each do |char|
-    if char == start_bracket
-      stack << start_bracket
-    elsif char == end_bracket
-      return false if stack.empty?
+    if char =~ OPEN_PATTERN
+      stack << char
+    elsif char =~ CLOSE_PATTERN
+      if stack.empty? || BRACKETS[stack.last] != char
+        return false
+      end
       stack.pop
     end
   end

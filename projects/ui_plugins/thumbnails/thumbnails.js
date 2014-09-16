8bdcb78568d59656b82b6c22563258ea71f88ebf
Will Hastings <will@objectdotcreate.net>
2014-09-16 12:24:02 -0700

    Add click switching of images in Thumbnails UI.

--- projects/ui_plugins/thumbnails/thumbnails.js
+++ projects/ui_plugins/thumbnails/thumbnails.js
@@ -7,8 +7,9 @@
     this.element = element;
     element.classList.add('thumbs-ui');
     this.images = Array.prototype.slice.call(element.querySelectorAll('img'));
-    this.activeImage = this.images[0];
-    this.activeImage.classList.add('thumbs-active');
+    this.activeIndex = 0;
+    this.images[0].classList.add('thumbs-active');
+    this.thumbImages = [];
 
     processImages.call(this);
     createThumbs.call(this);
@@ -18,10 +19,11 @@
   window.thumbnails = function(element) {
     var ui = Object.create(ThumbnailsUI);
     ui.init(element);
+    return ui;
   };
 
   var bindEvents = function() {
-
+    this.element.addEventListener('click', setActive.bind(this));
   };
 
   var calculateThumbWidth = function() {
@@ -35,6 +37,18 @@
     return newImage;
   };
 
+  var createThumb = function(image, index, thumbWidth) {
+    var thumbImage = copyImage(image);
+    if (index === 0) {
+      thumbImage.classList.add('thumbs-active');
+    }
+    thumbImage.classList.add('thumbs-thumb-img');
+    thumbImage.style.width = thumbWidth + 'px';
+    thumbImage.setAttribute('data-index', index);
+    this.thumbImages.push(thumbImage);
+    return thumbImage;
+  };
+
   var createThumbs = function() {
     var listEl = document.createElement('ul'),
         thumbWidth = calculateThumbWidth.call(this);
@@ -42,15 +56,10 @@
 
     this.images.forEach(function(image, index) {
       var li = document.createElement('li'),
-          thumbImage = copyImage(image);
-      if (index === 0) {
-        thumbImage.classList.add('thumbs-active');
-      }
-      thumbImage.classList.add('thumbs-thumb-img');
-      thumbImage.style.width = thumbWidth + 'px';
+          thumbImage = createThumb.call(this, image, index, thumbWidth);
       li.appendChild(thumbImage);
       listEl.appendChild(li);
-    });
+    }, this);
     listEl.style.width = (this.images.length * (thumbWidth + 10)) + 'px';
 
     this.element.appendChild(listEl);
@@ -60,6 +69,27 @@
     this.images.forEach(function(image) {
       image.classList.add('thumbs-image');
     });
+  };
+
+  var setActive = function(event) {
+    var thumbImage = event.target;
+    if (!thumbImage.classList.contains('thumbs-thumb-img')) {
+      return;
+    }
+
+    var newIndex = thumbImage.getAttribute('data-index'),
+        oldIndex = this.activeIndex,
+        oldImages = [this.images[oldIndex], this.thumbImages[oldIndex]],
+        newImages = [this.images[newIndex], this.thumbImages[newIndex]];
+
+    oldImages.forEach(function(image) {
+      image.classList.remove('thumbs-active');
+    });
+    newImages.forEach(function(image) {
+      image.classList.add('thumbs-active');
+    });
+
+    this.activeIndex = newIndex;
   };
 
 })(document);

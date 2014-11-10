a7397d1c0240cdfa7d7217de55e6a326daac056e
Will Hastings <will@objectdotcreate.net>
2014-11-09 16:36:56 -0800

    Finish zoom box movement for Zoomable UI.

--- projects/ui_plugins/zoomable/zoomable.js
+++ projects/ui_plugins/zoomable/zoomable.js
@@ -5,6 +5,13 @@
       CLASS_IMAGE_CONTAINER = 'zoomable-image-container',
       CLASS_ZOOMING = 'zoomable-zooming';
 
+  var BOUND_FUNCTIONS = {
+    moveZoom: moveZoom,
+    removeZoom: removeZoom,
+    setBoxPosition: setBoxPosition,
+    zoomImage: zoomImage
+  };
+
   var ZoomableUI = {};
 
   ZoomableUI.init = function(image, zoomArea) {
@@ -12,6 +19,8 @@
     this.zoomArea = zoomArea;
     this.container = image.parentNode;
     this.isBoxMoving = false;
+    this.boundFunctions = bindFunctions.call(this);
+    this.deltas = {};
 
     this.container.classList.add(CLASS_IMAGE_CONTAINER);
     image.addEventListener('load', addZoomBox.bind(this));
@@ -24,11 +33,10 @@
   };
 
   ZoomableUI.attachEventListeners = function() {
-    var image = this.image;
+    var image = this.image,
+        boundFunctions = this.boundFunctions;
 
-    image.addEventListener('mouseenter', zoomImage.bind(this));
-    image.addEventListener('mouseleave', removeZoom.bind(this));
-    image.addEventListener('mousemove', moveZoom.bind(this));
+    image.addEventListener('mouseenter', boundFunctions.zoomImage);
   };
 
   /* Event Listeners */
@@ -37,18 +45,25 @@
     if (this.isBoxMoving) {
       return;
     }
-    window.requestAnimationFrame(setBoxPosition.bind(this, event));
+    this.lastEvent = event;
+    window.requestAnimationFrame(this.boundFunctions.setBoxPosition);
+    this.isBoxMoving = true;
   }
 
-  function removeZoom(event) {
-    if (event.relatedTarget === this.zoomBox) {
-      return;
-    }
-    this.container.classList.remove(CLASS_ZOOMING);
+  function removeZoom() {
+    var container = this.container;
+
+    container.classList.remove(CLASS_ZOOMING);
+    container.removeEventListener('mousemove', this.boundFunctions.moveZoom);
+    container.removeEventListener('mouseleave', this.boundFunctions.removeZoom);
   }
 
   function zoomImage(event) {
-    this.container.classList.add(CLASS_ZOOMING);
+    var container = this.container;
+
+    container.classList.add(CLASS_ZOOMING);
+    container.addEventListener('mousemove', this.boundFunctions.moveZoom);
+    container.addEventListener('mouseleave', this.boundFunctions.removeZoom);
   }
 
   /* Helpers */
@@ -64,27 +79,75 @@
     zoomBox.style.width = width + 'px';
 
     this.image.parentNode.appendChild(zoomBox);
+    this.imgRect = this.image.getBoundingClientRect();
   }
 
-  function setBoxPosition(event) {
-    var zoomBox = this.zoomBox,
-        boxOrigPos,
-        boxOffset,
-        topDelta,
-        leftDelta;
+  function bindFunctions() {
+    var boundFunctions = {};
+    Object.keys(BOUND_FUNCTIONS).forEach(function(funcName) {
+      boundFunctions[funcName] = BOUND_FUNCTIONS[funcName].bind(this);
+    }, this);
+    return boundFunctions;
+  }
 
-    this.boxOrigPos = this.boxOrigPos || zoomBox.getBoundingClientRect();
-    boxOrigPos = this.boxOrigPos;
+  function setBoxData() {
+    this.boxOrigPos = this.boxOrigPos || this.zoomBox.getBoundingClientRect();
     this.boxOffset = this.boxOffset || {
-      height: boxOrigPos.height / 2,
-      width: boxOrigPos.width / 2
+      height: this.boxOrigPos.height / 2,
+      width: this.boxOrigPos.width / 2
     };
-    boxOffset = this.boxOffset;
+  }
 
-    topDelta = (event.clientY - boxOrigPos.top - boxOffset.height) + 'px',
-    leftDelta = (event.clientX - boxOrigPos.left - boxOffset.width) + 'px';
-    zoomBox.style.transform = 'translate3d(' + leftDelta + ', ' + topDelta + ', 0)';
+  function setDeltas() {
+    var boxOrigPos = this.boxOrigPos,
+        boxOffset = this.boxOffset,
+        imgRect = this.imgRect,
+        event = this.lastEvent,
+        leftDelta,
+        topDelta,
+        fromImgBottom = imgRect.bottom - boxOrigPos.height,
+        fromImgRight = imgRect.right - boxOrigPos.width;
+
+    topDelta = event.clientY - boxOffset.height;
+    leftDelta = event.clientX - boxOffset.width;
+
+    if (topDelta > fromImgBottom) {
+      topDelta = fromImgBottom;
+    } else if (topDelta < imgRect.top) {
+      topDelta = imgRect.top;
+    }
+
+    if (leftDelta > fromImgRight) {
+      leftDelta = fromImgRight;
+    } else if (leftDelta < imgRect.left) {
+      leftDelta = imgRect.left;
+    }
+
+    this.deltas.top = topDelta - boxOrigPos.top;
+    this.deltas.left = leftDelta - boxOrigPos.left;
+    return this.deltas;
+  }
+
+  function isOutsideImage(x, y) {
+    var imgRect = this.imgRect;
+
+    return x > imgRect.right || x < imgRect.left ||
+      y > imgRect.bottom || y < imgRect.top;
+  }
+
+  function setBoxPosition() {
+    var event = this.lastEvent,
+        deltas;
+
     this.isBoxMoving = false;
+    if (isOutsideImage.call(this, event.clientX, event.clientY)) {
+      return removeZoom.call(this);
+    }
+
+    setBoxData.call(this);
+    deltas = setDeltas.call(this);
+    this.zoomBox.style.transform =
+      'translate3d(' + deltas.left + 'px, ' + deltas.top + 'px, 0)';
   }
 
 })(document);

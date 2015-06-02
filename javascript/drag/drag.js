a98f9622efd6e25699f86504d840eefb075ade22
Will Hastings <will@objectdotcreate.net>
2015-06-01 17:04:06 -0700

    Add element dragging to JS.

---
+++ javascript/drag/drag.js
@@ -0,0 +1,37 @@
+(function() {
+  'use strict';
+
+  var dragEl = document.querySelector('.drag-el'),
+      dragElOffsetX = dragEl.clientWidth / 2,
+      dragElOffsetY = dragEl.clientHeight / 2,
+      isMovePending = false,
+      parentPos = dragEl.parentNode.getBoundingClientRect();
+
+  function addMouseListeners() {
+    dragEl.addEventListener('mousemove', scheduleMove);
+  }
+
+  function removeMouseListeners() {
+    dragEl.removeEventListener('mousemove', scheduleMove);
+  }
+
+  function scheduleMove(event) {
+    if (!isMovePending) {
+      window.requestAnimationFrame(moveEl.bind(null, event));
+      isMovePending = true;
+    }
+  }
+
+  function moveEl(event) {
+    var mouseX = event.clientX,
+        mouseY = event.clientY,
+        offsetX = mouseX - parentPos.left - dragElOffsetX,
+        offsetY = mouseY - parentPos.top - dragElOffsetY;
+
+    dragEl.style.transform = 'translate3d(' + offsetX + 'px, ' + offsetY + 'px, 0)';
+    isMovePending = false;
+  }
+
+  dragEl.addEventListener('mousedown', addMouseListeners);
+  document.body.addEventListener('mouseup', removeMouseListeners);
+})();

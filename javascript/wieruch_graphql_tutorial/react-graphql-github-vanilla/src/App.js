72b195e9eb95fedb8a1cd763910f8d1829c4dacc
Will Hastings <will@objectdotcreate.net>
2018-08-14 11:51:45 -0700

    Add start of Wieruch's 'React with GraphQL' tutorial.

---
+++ javascript/wieruch_graphql_tutorial/react-graphql-github-vanilla/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const TITLE = 'React GraphQL GitHub Client';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{TITLE}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;

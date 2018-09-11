e6f7d5d030c96b5a615cf667f50a3fb85cef4b6b
Will Hastings <will.hastings@omadahealth.com>
2018-09-11 12:26:38 -0700

    Break out Apollo client setup code in Wieruch's 'React with GraphQL' tutorial.

--- javascript/wieruch_graphql_tutorial/apollo-react-github/src/index.js
+++ javascript/wieruch_graphql_tutorial/apollo-react-github/src/index.js
@@ -1,45 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
-import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-const GITHUB_BASE_URL = 'https://api.github.com/graphql';
-
-const httpLink = new HttpLink({
-  uri: GITHUB_BASE_URL,
-  headers: {
-    authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
-  },
-});
-
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    console.warn('GRAPHQL ERRORS:');
-    console.warn(graphQLErrors);
-  }
-
-  if (networkError) {
-    console.warn('NETWORK ERRORS:');
-    console.warn(graphQLErrors);
-  }
-});
-
-const link = ApolloLink.from([errorLink, httpLink]);
-
-const cache = new InMemoryCache();
-
-const client = new ApolloClient({
-  cache,
-  link,
-});
+import client from './apolloClient';
 
 ReactDOM.render(
   <ApolloProvider client={client}>

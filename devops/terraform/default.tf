601e086a55d9aecce51ddd912ab12d1320210bea
Will Hastings <will.hastings@omadahealth.com>
2019-02-07 17:43:58 -0800

    Add security group via Terraform so I can ping my instance

--- devops/terraform/default.tf
+++ devops/terraform/default.tf
@@ -4,9 +4,22 @@ provider "aws" {
   region = "${var.region}"
 }
 
+resource "aws_security_group" "default" {
+  name = "terraform_practice"
+
+  # Allow pings
+  ingress {
+    protocol = "icmp"
+    from_port = 8
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
   ami = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
 
   provisioner "local-exec" {
     command = "echo ${aws_instance.example.public_ip} > ip_address.txt"

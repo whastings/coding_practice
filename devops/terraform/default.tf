a8d3409e28db95243ab64eaa8e577ab1edeee937
Will Hastings <will.hastings@omadahealth.com>
2019-02-07 16:34:44 -0800

    Do variables and outputs sections from Terraform Getting Started tutorial

--- devops/terraform/default.tf
+++ devops/terraform/default.tf
@@ -1,14 +1,18 @@
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region = "us-west-1"
+  region = "${var.region}"
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-693d4009"
+  ami = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
 
   provisioner "local-exec" {
     command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
   }
+}
+
+output "host" {
+  value = "${aws_instance.example.public_dns}"
 }
--- devops/terraform/variables.tf
+++ devops/terraform/variables.tf
@@ -1,2 +1,12 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+variable "region" {
+  default = "us-west-1"
+}
+variable "amis" {
+  type = "map"
+  default = {
+    "us-east-1" = "ami-b374d5a5"
+    "us-west-1" = "ami-693d4009"
+  }
+}

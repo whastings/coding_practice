23aaeeff37c1ce3f9486af89bc1e86be86c3a584
Will Hastings <will.hastings@omadahealth.com>
2019-04-25 17:14:06 -0700

    Egghead React Native tutorial: Create initial list of restaurants

--- javascript/react-native/RestaurantReview/App.js
+++ javascript/react-native/RestaurantReview/App.js
@@ -1,49 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+const restaurants = [
+  { name: 'React Cafe', address: '123 Anywhere St' },
+  { name: 'Fancy Restaurant', address: '799 Main St' },
+  { name: 'Taco Place', address: '550 Maple Rd' },
+];
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View
+        style={{
+          flex: 1,
+        }}
+      >
+        <Text style={{
+          padding: 40,
+          fontSize: 30,
+          textAlign: 'center',
+          color: '#0066CC',
+          fontWeight: '300',
+        }}>
+          Restaurant Review!
+        </Text>
+
+        {restaurants.map((place, index) => {
+          return (
+            <View
+              key={place.name}
+              style={{
+                flexDirection: 'row',
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <Text>{index + 1}</Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: 'column',
+                  flex: 8,
+                }}
+              >
+                <Text>{place.name}</Text>
+                <Text
+                  style={{
+                    color: 'grey'
+                  }}
+                >
+                  {place.address}
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <Text>Info</Text>
+              </View>
+            </View>
+          );
+        })}
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});

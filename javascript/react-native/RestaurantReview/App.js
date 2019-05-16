219701476545d33ba4da321ac45ef07b05fb45f8
Will Hastings <will.hastings@omadahealth.com>
2019-05-16 16:23:13 -0700

    Egghead React Native tutorial: Wrap restaurant list in scroll view

--- javascript/react-native/RestaurantReview/App.js
+++ javascript/react-native/RestaurantReview/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 import Header from './src/components/Header';
 
@@ -7,6 +7,23 @@ const restaurants = [
   { name: 'React Cafe', address: '123 Anywhere St' },
   { name: 'Fancy Restaurant', address: '799 Main St' },
   { name: 'Taco Place', address: '550 Maple Rd' },
+  { name: "Tony's Diner", address: '4101 College St' },
+  { name: 'Pasta Central', address: '706 Harper St' },
+  { name: 'Burger Builder', address: '4869 Hamilton Dr' },
+  { name: 'Pizza Express', address: '1049 Bird St' },
+  { name: 'Teriyaki To Go', address: '1885 Tea Berry Lane' },
+  { name: 'Maroon Deli', address: '1082 Stuart St' },
+  { name: 'Prime Bar and Grill', address: '1848 Fairfax Dr' },
+  { name: 'Dumpling House', address: '747 Kelly Dr' },
+  { name: 'Hot Chicken', address: '1816 Olive St' },
+  { name: "Luna's Tap Room", address: '3256 Spirit Dr' },
+  { name: 'Quick Sandwich Shop', address: '2587 Cherry Ridge Dr' },
+  { name: "Bobby's Burgers", address: '4152 Berkley St' },
+  { name: 'Turnpike Diner', address: '4571 Central Ave' },
+  { name: 'Bombay Express', address: '65 Queens Lane' },
+  { name: 'Coffee Central', address: '3228 Oakwood Circle' },
+  { name: "King's Garden", address: '2935 Victoria Ct' },
+  { name: 'Salads and More', address: '2454 Preston St' },
 ];
 
 export default class App extends Component {
@@ -47,30 +64,32 @@ export default class App extends Component {
           value={search}
         />
 
-        {this.restaurantsToRender().map((place, index) => {
-          return (
-            <View
-              key={place.name}
-              style={[
-                styles.row,
-                { backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7' },
-              ]}
-            >
-              <View style={styles.edges}>
-                <Text>{index + 1}</Text>
+        <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+          {this.restaurantsToRender().map((place, index) => {
+            return (
+              <View
+                key={place.name}
+                style={[
+                  styles.row,
+                  { backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7' },
+                ]}
+              >
+                <View style={styles.edges}>
+                  <Text>{index + 1}</Text>
+                </View>
+                <View style={styles.nameAndAddress}>
+                  <Text>{place.name}</Text>
+                  <Text style={styles.addressText}>
+                    {place.address}
+                  </Text>
+                </View>
+                <View style={styles.edges}>
+                  <Text>Info</Text>
+                </View>
               </View>
-              <View style={styles.nameAndAddress}>
-                <Text>{place.name}</Text>
-                <Text style={styles.addressText}>
-                  {place.address}
-                </Text>
-              </View>
-              <View style={styles.edges}>
-                <Text>Info</Text>
-              </View>
-            </View>
-          );
-        })}
+            );
+          })}
+        </ScrollView>
       </View>
     );
   }
@@ -103,7 +122,6 @@ const styles = StyleSheet.create({
     color: 'grey'
   },
   input: {
-    marginBottom: 30,
     padding: 10,
     paddingHorizontal: 20,
     fontSize: 16,
@@ -111,5 +129,8 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: '#ddd',
     backgroundColor: '#F5F5F5',
+  },
+  scrollViewContainer: {
+    paddingTop: 30,
   },
 });

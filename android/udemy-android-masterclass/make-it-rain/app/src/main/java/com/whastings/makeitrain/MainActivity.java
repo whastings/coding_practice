fe11c8497cf55f1eded998903fd766f246cfe185
Will Hastings <will@objectdotcreate.net>
2019-06-06 17:11:08 -0700

    Udemy Android Masterclass: Wire up Make It Rain button

--- android/udemy-android-masterclass/make-it-rain/app/src/main/java/com/whastings/makeitrain/MainActivity.java
+++ android/udemy-android-masterclass/make-it-rain/app/src/main/java/com/whastings/makeitrain/MainActivity.java
@@ -2,12 +2,30 @@ package com.whastings.makeitrain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView moneyText;
+    private int moneyCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        moneyText = findViewById(R.id.money_text);
+    }
+
+    public void showTag(View v) {
+        Log.d("MYTAG", "onClick: Show Tag");
+    }
+
+    public void makeItRain(View v) {
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+        moneyCounter += 1000;
+        moneyText.setText(String.valueOf(numberFormat.format(moneyCounter)));
     }
 }

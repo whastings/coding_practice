5a14c8d3e2ad916970dbd4bd81b2434b6327100f
Will Hastings <will.hastings@omadahealth.com>
2019-06-27 16:51:40 -0700

    Udemy Android Masterclass: Add toast and alert text

--- android/udemy-android-masterclass/make-it-rain/app/src/main/java/com/whastings/makeitrain/MainActivity.java
+++ android/udemy-android-masterclass/make-it-rain/app/src/main/java/com/whastings/makeitrain/MainActivity.java
@@ -2,14 +2,15 @@ package com.whastings.makeitrain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
     private TextView moneyText;
+    private TextView richText;
     private int moneyCounter = 0;
 
     @Override
@@ -17,15 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         moneyText = findViewById(R.id.money_text);
+        richText = findViewById(R.id.rich_text);
     }
 
     public void showTag(View v) {
-        Log.d("MYTAG", "onClick: Show Tag");
+        Toast.makeText(getApplicationContext(), R.string.app_name, Toast.LENGTH_SHORT)
+                .show();
     }
 
     public void makeItRain(View v) {
         NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
         moneyCounter += 1000;
         moneyText.setText(String.valueOf(numberFormat.format(moneyCounter)));
+
+        if (moneyCounter > 9999.0f) {
+            richText.setVisibility(View.VISIBLE);
+        }
     }
 }

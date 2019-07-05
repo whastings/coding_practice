3e03bce973ab8cc7eb79e6ff82c9e9a084876cfe
Will Hastings <will@objectdotcreate.net>
2019-07-05 12:05:57 -0700

    Udemy Android Masterclass: Implement Quiz app next question functionality

--- android/udemy-android-masterclass/true-citizen-quiz/app/src/main/java/com/example/truecitizenquiz/MainActivity.java
+++ android/udemy-android-masterclass/true-citizen-quiz/app/src/main/java/com/example/truecitizenquiz/MainActivity.java
@@ -5,13 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button falseButton;
     private Button trueButton;
+    private ImageButton nextButton;
     private TextView questionTextView;
+    private Question[] questionBank = new Question[] {
+            new Question(R.string.question_constitution, true),
+            new Question(R.string.question_declaration, true),
+            new Question(R.string.question_independence_rights, true),
+            new Question(R.string.question_religion, true),
+            new Question(R.string.question_government, false),
+            new Question(R.string.question_government_feds, false),
+            new Question(R.string.question_government_senators, true),
+    };
+    private int currentQuestionIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +32,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         falseButton = findViewById(R.id.false_button);
         trueButton = findViewById(R.id.true_button);
+        nextButton = findViewById(R.id.next_button);
         questionTextView = findViewById(R.id.answer_text_view);
 
         falseButton.setOnClickListener(this);
         trueButton.setOnClickListener(this);
+        nextButton.setOnClickListener(this);
     }
 
     @Override
@@ -34,6 +48,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.true_button:
                 Toast.makeText(MainActivity.this, "True", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.next_button:
+                currentQuestionIndex = (currentQuestionIndex + 1) % questionBank.length;
+                questionTextView.setText(questionBank[currentQuestionIndex].getAnswerResId());
                 break;
         }
     }

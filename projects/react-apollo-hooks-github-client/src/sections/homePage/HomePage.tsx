9b6f3548263128e307ce0138dfd54612b29b6355
Will Hastings <will@objectdotcreate.net>
2019-09-29 15:42:26 -0700

    react-apollo-hooks-github-client: Add load more repos button to Home

--- projects/react-apollo-hooks-github-client/src/sections/homePage/HomePage.tsx
+++ projects/react-apollo-hooks-github-client/src/sections/homePage/HomePage.tsx
@@ -1,54 +1,31 @@
 import React from 'react'
-import gql from 'graphql-tag'
-import { useQuery } from 'react-apollo'
 
-import { OwnReposQuery, OwnReposQueryVariables } from './types/OwnReposQuery'
-
-export const OWN_REPOS_QUERY = gql`
-  query OwnReposQuery($first: Int!, $after: String) {
-    viewer {
-      repositories(first: $first, after: $after) {
-        nodes {
-          name
-          owner {
-            login
-          }
-        }
-        pageInfo {
-          endCursor
-          hasNextPage
-        }
-      }
-    }
-  }
-`
+import { useOwnReposQuery } from './homePageHooks'
 
 const HomePage: React.FC = () => {
-  const { data, loading } = useQuery<OwnReposQuery, OwnReposQueryVariables>(
-    OWN_REPOS_QUERY,
-    {
-      variables: { first: 5 },
-    }
-  )
+  const { fetchMore, hasNextPage, loading, loadingMore, repos } = useOwnReposQuery()
 
-  if (loading || !data) {
+  if (loading || !repos) {
     return (
       <strong>Loading...</strong>
     )
   }
 
-  const ownRepos = data.viewer.repositories.nodes || []
-
   return (
     <>
       <h1>Your Repos</h1>
       <ul>
-        {ownRepos.map((repo) => (
+        {repos.map((repo) => (
           <li key={repo!.name}>
             {repo!.name}
           </li>
         ))}
       </ul>
+      {hasNextPage && (
+        <button onClick={fetchMore} disabled={loadingMore}>
+          Load More
+        </button>
+      )}
     </>
   )
 }

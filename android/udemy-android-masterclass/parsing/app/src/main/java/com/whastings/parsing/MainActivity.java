6743debc5d9b3787973c6fdf3850731ec8306313
Will Hastings <will@objectdotcreate.net>
2019-10-19 10:53:59 -0700

    Udemy Android Masterclass: Fetch JSON array with Volley

--- android/udemy-android-masterclass/parsing/app/src/main/java/com/whastings/parsing/MainActivity.java
+++ android/udemy-android-masterclass/parsing/app/src/main/java/com/whastings/parsing/MainActivity.java
@@ -9,9 +9,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -47,6 +49,32 @@ public class MainActivity extends AppCompatActivity {
             }
         );
 
-        requestQueue.add(jsonObjectRequest);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                "https://jsonplaceholder.typicode.com/todos",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                Log.d("JSON Response: ", "title: " + jsonObject.getString("title"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("JSON ERROR: ", error.getMessage());
+                    }
+                }
+        );
+
+        requestQueue.add(jsonArrayRequest);
+        // requestQueue.add(jsonObjectRequest);
     }
 }

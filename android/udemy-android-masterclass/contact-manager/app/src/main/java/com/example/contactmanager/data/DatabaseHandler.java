b9efda7d52b9bf85dbbdaa015e75cb49b50ea210
Will Hastings <will@objectdotcreate.net>
2019-11-16 17:50:01 -0800

    Udemy Android Masterclass: Add get contact and add contact in Contact Manager

--- android/udemy-android-masterclass/contact-manager/app/src/main/java/com/example/contactmanager/data/DatabaseHandler.java
+++ android/udemy-android-masterclass/contact-manager/app/src/main/java/com/example/contactmanager/data/DatabaseHandler.java
@@ -1,10 +1,13 @@
 package com.example.contactmanager.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.example.contactmanager.model.Contact;
 import com.example.contactmanager.util.Util;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -26,5 +29,39 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         String dropTable = "DROP TABLE IF EXISTS " + Util.TABLE_NAME;
         db.execSQL(dropTable);
         onCreate(db);
+    }
+
+    public void addContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Util.KEY_NAME, contact.getName());
+        values.put(Util.KEY_PHONE_NUMBER, contact.getPhoneNumber());
+
+        db.insert(Util.TABLE_NAME, null, values);
+        db.close();
+    }
+
+    public Contact getContact(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(
+                Util.TABLE_NAME,
+                new String[]{ Util.KEY_ID, Util.KEY_NAME, Util.KEY_PHONE_NUMBER },
+                Util.KEY_ID + "= ?",
+                new String[] { String.valueOf(id) },
+                null, null, null
+        );
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+            Contact contact = new Contact();
+            contact.setId(Integer.parseInt(cursor.getString(0)));
+            contact.setName(cursor.getString(1));
+            contact.setPhoneNumber(cursor.getString(2));
+
+            return contact;
+        }
+
+        return null;
     }
 }

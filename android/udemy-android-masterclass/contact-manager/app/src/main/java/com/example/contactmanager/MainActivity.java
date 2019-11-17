4a700dafb4118f5c34ba96ef8ed0fd7c1afc02ce
Will Hastings <will@objectdotcreate.net>
2019-11-17 11:45:29 -0800

    Udemy Android Masterclass: Display contacts list in ListView

--- android/udemy-android-masterclass/contact-manager/app/src/main/java/com/example/contactmanager/MainActivity.java
+++ android/udemy-android-masterclass/contact-manager/app/src/main/java/com/example/contactmanager/MainActivity.java
@@ -5,18 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.contactmanager.data.DatabaseHandler;
 import com.example.contactmanager.model.Contact;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button addButton;
     private Button deleteButton;
     private Button updateButton;
+    private ListView contactsListView;
+
+    private ArrayList<String> contactStringsList;
+    private ArrayAdapter<String> contactsArrayAdapter;
     private DatabaseHandler dbHandler;
     private TextView contactsCountText;
 
@@ -31,15 +39,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         deleteButton.setOnClickListener(this);
         updateButton = findViewById(R.id.update_button);
         updateButton.setOnClickListener(this);
+        contactsListView = findViewById(R.id.contacts_list);
+
+        contactStringsList = new ArrayList<>();
 
         dbHandler = new DatabaseHandler(MainActivity.this);
         List<Contact> contactList = dbHandler.getAllContacts();
         for (Contact contact : contactList) {
             Log.d("MainActivity", "contact: " + contact.getId() + " " + contact.getName());
+            contactStringsList.add(contact.getName());
         }
+        contactsArrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                contactStringsList
+        );
+        contactsListView.setAdapter(contactsArrayAdapter);
 
         contactsCountText = findViewById(R.id.contacts_count_text);
         contactsCountText.setText("Contacts: " + dbHandler.getCount());
+        contactsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Log.d("MainActivity", "Contact Clicked: " + contactStringsList.get(position));
+            }
+        });
     }
 
     @Override
@@ -58,10 +82,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void addContacts() {
-        Contact jeremy = new Contact("Jeremy", "1112223333");
-        dbHandler.addContact(jeremy);
-        Contact jason = new Contact("Jason", "5556667777");
-        dbHandler.addContact(jason);
+        for (int i = 1; i <= 20; i++) {
+            Contact contact = new Contact("Contact " + i, i + "000000");
+            dbHandler.addContact(contact);
+        }
     }
 
     private void deleteContacts() {

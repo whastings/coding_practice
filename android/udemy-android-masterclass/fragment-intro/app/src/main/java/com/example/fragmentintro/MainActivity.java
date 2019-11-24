d793d6756e4dbf7f42a84f557f61c7cbd75cc76e
Will Hastings <will@objectdotcreate.net>
2019-11-23 20:30:33 -0800

    Udemy Android Masterclass: Create fragment programmatically

--- android/udemy-android-masterclass/fragment-intro/app/src/main/java/com/example/fragmentintro/MainActivity.java
+++ android/udemy-android-masterclass/fragment-intro/app/src/main/java/com/example/fragmentintro/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fragmentintro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.main_layout);
+
+        if (fragment == null) {
+            fragment = new MainFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.main_layout, fragment)
+                    .commit();
+        }
     }
 }

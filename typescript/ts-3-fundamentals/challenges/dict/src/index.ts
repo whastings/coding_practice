0ff9a5b80f40a25fc694feb17dd5e5603b8da756
Will Hastings <will@objectdotcreate.net>
2019-12-24 15:56:23 -0800

    ts-3-fundamentals: Solve Dict challenge

--- typescript/ts-3-fundamentals/challenges/dict/src/index.ts
+++ typescript/ts-3-fundamentals/challenges/dict/src/index.ts
@@ -1,7 +1,25 @@
-export type Dict = {};
+export type Dict<T = any> = {
+  [prop: string]: T,
+}
 
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+type MapCallback<T, U> = (value: T) => U
+export function mapDict<T, U>(dict: Dict<T>, callback: MapCallback<T, U>): Dict<U> {
+  return Object.keys(dict)
+    .filter((key) => dict[key] !== undefined)
+    .reduce((newDict, key) => {
+      newDict[key] = callback(dict[key])
+      return newDict
+    }, {} as Dict)
+}
 
 // Array.prototype.reduce, but for Dict
-export function reduceDict() {}
+type ReduceCallback<T, U> = (value: T, accumulator: U) => U
+export function reduceDict<T, U>(dict: Dict<T>, callback: ReduceCallback<T, U>, initialValue: U): U {
+  return Object.keys(dict).reduce((accumulator, key) => {
+    if (dict[key] === undefined) {
+      return accumulator
+    }
+    return callback(dict[key], accumulator)
+  }, initialValue)
+}

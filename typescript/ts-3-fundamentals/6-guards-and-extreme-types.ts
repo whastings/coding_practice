2b84473d729c93ff94018285886f423cb0792e05
Will Hastings <will@objectdotcreate.net>
2019-12-25 14:11:03 -0800

    ts-3-fundamentals: Top types and type guards

---
+++ typescript/ts-3-fundamentals/6-guards-and-extreme-types.ts
@@ -0,0 +1,40 @@
+import { HasEmail } from "./1-basics"
+
+// Top types can hold any value
+// They are 'any` and `unknown`
+const myAny: any = 10
+const myUnknown: unknown = 'hello'
+// You can do anything with `any`
+myAny.foo.bar.baz
+// But nothing with `unknown`
+// myUnknown.foo (Error)
+
+// Can use `any` when you don't care about the type of something
+const logPromise = async (promise: Promise<any>) => {
+  console.log(await promise)
+}
+
+// Type guards confirm a value is of a given type so you can use it as such
+// `typeof` is a type guard good for primitives
+if (typeof myUnknown === 'string') {
+  myUnknown.split(' ')
+}
+// `instanceof` is a type guard good for objects
+if (myUnknown instanceof Promise) {
+  logPromise(myUnknown)
+}
+
+// User defined type guards are custom ones you create
+const isHasEmail = (value: any): value is HasEmail => {
+  // Should return true if value is type or false if it isn't
+  return typeof value.name === 'string' && typeof value.email === 'string'
+}
+if (isHasEmail(myUnknown)) {
+  myUnknown.email
+}
+// Example: Checking if something is not `undefined`
+const isDefined = <T>(arg: T | undefined): arg is T => {
+  return typeof arg !== 'undefined'
+}
+const arrayWithUndefined = [1, 2, undefined, 3]
+const arrayWithoutUndefined = arrayWithUndefined.filter(isDefined)

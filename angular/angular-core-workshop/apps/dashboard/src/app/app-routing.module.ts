f8485003a485a46eb656dab45ae3e6e87ed4dfce
Will Hastings <will@objectdotcreate.net>
2019-12-30 12:51:57 -0800

    angular-core-workshop: Hook up other routes with lazy loading

--- angular/angular-core-workshop/apps/dashboard/src/app/app-routing.module.ts
+++ angular/angular-core-workshop/apps/dashboard/src/app/app-routing.module.ts
@@ -4,9 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '@workshop/ui-login'
 
 
+// Matched component will render into <router-outlet> in app.component.html
 const routes: Routes = [
-  // Matched component will render into <router-outlet> in app.component.html
-  { path: '', component: LoginComponent },
+  // `loadChildren` will cause code for that route to be lazy-loaded
+  // Then that module's routing module will figure out which component to render
+  { path: '', loadChildren: './home/home.module#HomeModule' },
+  { path: 'projects', loadChildren: './projects/projects.module#ProjectsModule' },
+  { path: 'customers', loadChildren: './customers/customers.module#CustomersModule' },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 

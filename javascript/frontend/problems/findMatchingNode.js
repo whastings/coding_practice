bf8fce3d3a4369beb1cafd2c87cac2b9c8842f94
Will Hastings <will@objectdotcreate.net>
2020-02-08 13:06:00 -0800

    javascript: findMatchingNodes

--- javascript/frontend/problems/findMatchingNode.js
+++ javascript/frontend/problems/findMatchingNode.js
@@ -1,4 +1,4 @@
-const findMatchingNode = (searchNode, domTree2) => {
+export const findMatchingNode = (searchNode, domTree2) => {
   const childrenIndexes = []
 
   let currentNode = searchNode
@@ -14,4 +14,41 @@ const findMatchingNode = (searchNode, domTree2) => {
   }, domTree2)
 }
 
-export default findMatchingNode
+export const findMatchingNodes = (searchNodes, domTree2) => {
+  const nodeDepths = searchNodes.map((searchNode) => {
+    let depth = 0
+    let currentNode = searchNode
+    while (currentNode.parentNode) {
+      depth += 1
+      currentNode = currentNode.parentNode
+    }
+    return { depth, node: searchNode }
+  })
+  const deepestNode = nodeDepths
+    .sort((depth1, depth2) => depth1.depth - depth2.depth)
+    .pop()
+    .node
+
+  const nodePathsMap = searchNodes.reduce((map, searchNode) => {
+    map.set(searchNode, [])
+    return map
+  }, new Map())
+
+  let currentNode = deepestNode
+  while (currentNode.parentNode) {
+    const { parentNode } = currentNode
+    parentNode.childNodes.forEach((node, i) => {
+      if (nodePathsMap.has(node)) {
+        nodePathsMap.get(node).push(i)
+      }
+    })
+    currentNode = parentNode
+  }
+
+  return searchNodes.map((searchNode) => {
+    const nodePath = nodePathsMap.get(searchNode)
+    return nodePath.reduceRight((tree2Node, index) => {
+      return tree2Node.children[index]
+    }, domTree2)
+  })
+}
--- javascript/frontend/problems/findMatchingNode.test.js
+++ javascript/frontend/problems/findMatchingNode.test.js
@@ -1,24 +1,43 @@
-import findMatchingNode from './findMatchingNode'
+import { findMatchingNode, findMatchingNodes } from './findMatchingNode'
+
+const createDomTree = () => {
+  const domTree = document.createElement('div')
+  domTree.appendChild(document.createElement('div'))
+  domTree.appendChild(document.createElement('div'))
+  domTree.appendChild(document.createElement('div'))
+  const level2Node = domTree.children[2]
+  level2Node.appendChild(document.createElement('div'))
+  level2Node.appendChild(document.createElement('div'))
+  const level3Node = level2Node.children[1]
+  level3Node.appendChild(document.createElement('div'))
+
+  return domTree
+}
 
 describe('findMatchingNode()', () => {
-  const createDomTree = () => {
-    const domTree = document.createElement('div')
-    domTree.appendChild(document.createElement('div'))
-    domTree.appendChild(document.createElement('div'))
-    domTree.appendChild(document.createElement('div'))
-    const level2Node = domTree.children[2]
-    level2Node.appendChild(document.createElement('div'))
-    level2Node.appendChild(document.createElement('div'))
-
-    return domTree
-  }
-
   it('given a node from one DOM tree, it finds the matching node in an identical tree', () => {
     const domTree1 = createDomTree()
     const domTree2 = createDomTree()
-    const searchNode = domTree1.children[2].children[0]
-    const expectedNode = domTree2.children[2].children[0]
+    const searchNode = domTree1.lastChild.firstChild
+    const expectedNode = domTree2.lastChild.firstChild
 
     expect(findMatchingNode(searchNode, domTree2)).toEqual(expectedNode)
+  })
+})
+
+describe('findMatchingNodes()', () => {
+  it('takes an array of nodes in one DOM tree and finds the matching nodes in another tree', () => {
+    const domTree1 = createDomTree()
+    const domTree2 = createDomTree()
+    const searchNode1 = domTree1.children[1]
+    const searchNode2 = domTree1.lastChild.firstChild
+    const searchNode3 = domTree1.lastChild.lastChild.firstChild
+    const expectedNode1 = domTree2.children[1]
+    const expectedNode2 = domTree2.lastChild.firstChild
+    const expectedNode3 = domTree2.lastChild.lastChild.firstChild
+
+    expect(
+      findMatchingNodes([searchNode1, searchNode2, searchNode3], domTree2)
+    ).toEqual([expectedNode1, expectedNode2, expectedNode3])
   })
 })

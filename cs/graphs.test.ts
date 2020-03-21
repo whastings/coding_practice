5885edc4123c49db1dd9a2ad70ac92da770baac6
Will Hastings <will@objectdotcreate.net>
2020-03-21 09:32:59 -0700

    cs: AdjacencyMatrixGraph

--- cs/graphs.test.ts
+++ cs/graphs.test.ts
@@ -1,4 +1,10 @@
-import { GraphNode, traverse } from './graphs'
+import {
+  GraphNode,
+  traverse,
+  AdjacencyNode,
+  AdjacencyListGraph,
+  AdjacencyMatrixGraph,
+} from './graphs'
 
 describe('traverse()', () => {
   const makeGraph = () => {
@@ -39,5 +45,83 @@ describe('traverse()', () => {
     }, 2)
 
     expect(result).toEqual(['a', 'b', 'c', 'd', 'e', 'f'])
+  })
+})
+
+describe('AdjacencyMatrixGraph', () => {
+  const makeGraph = () => {
+    const graph = new AdjacencyMatrixGraph()
+    const nodeA = new AdjacencyNode<string>('a')
+    const nodeB = new AdjacencyNode<string>('b')
+    const nodeC = new AdjacencyNode<string>('c')
+    const nodeD = new AdjacencyNode<string>('d')
+    const nodeE = new AdjacencyNode<string>('e')
+    const nodeF = new AdjacencyNode<string>('f')
+    const nodeG = new AdjacencyNode<string>('g')
+
+    const nodes = [nodeA, nodeB, nodeC, nodeD, nodeE, nodeF, nodeG]
+    nodes.forEach((node) => graph.addNode(node))
+    graph.addEdge(nodeA, nodeB)
+    graph.addEdge(nodeA, nodeC)
+    graph.addEdge(nodeB, nodeD)
+    graph.addEdge(nodeB, nodeE)
+    graph.addEdge(nodeC, nodeD)
+    graph.addEdge(nodeC, nodeF)
+    graph.addEdge(nodeE, nodeG)
+    graph.addEdge(nodeF, nodeG)
+
+    return { graph, nodeC, nodeD }
+  }
+
+  it('stores graph edges using a matrix', () => {
+    const { graph } = makeGraph()
+
+    expect(graph.toArray()).toEqual([
+      //     a   b   c   d   e   f   g
+      /*a*/ [0,  1,  1,  0,  0,  0,  0],
+      /*b*/ [1,  0,  0,  1,  1,  0,  0],
+      /*c*/ [1,  0,  0,  1,  0,  1,  0],
+      /*d*/ [0,  1,  1,  0,  0,  0,  0],
+      /*e*/ [0,  1,  0,  0,  0,  0,  1],
+      /*f*/ [0,  0,  1,  0,  0,  0,  1],
+      /*g*/ [0,  0,  0,  0,  1,  1,  0],
+    ])
+  })
+
+  describe('removeNode', () => {
+    it('removes a node and its connections from the graph', () => {
+      const { graph, nodeD } = makeGraph()
+
+      graph.removeNode(nodeD)
+
+      expect(graph.toArray()).toEqual([
+        //     a   b   c   e   f   g
+        /*a*/ [0,  1,  1,  0,  0,  0],
+        /*b*/ [1,  0,  0,  1,  0,  0],
+        /*c*/ [1,  0,  0,  0,  1,  0],
+        /*e*/ [0,  1,  0,  0,  0,  1],
+        /*f*/ [0,  0,  1,  0,  0,  1],
+        /*g*/ [0,  0,  0,  1,  1,  0],
+      ])
+    })
+  })
+
+  describe('removeEdge', () => {
+    it('removes an edge from between two nodes', () => {
+      const { graph, nodeC, nodeD } = makeGraph()
+
+      graph.removeEdge(nodeC, nodeD)
+
+      expect(graph.toArray()).toEqual([
+        //     a   b   c   d   e   f   g
+        /*a*/ [0,  1,  1,  0,  0,  0,  0],
+        /*b*/ [1,  0,  0,  1,  1,  0,  0],
+        /*c*/ [1,  0,  0,  0,  0,  1,  0],
+        /*d*/ [0,  1,  0,  0,  0,  0,  0],
+        /*e*/ [0,  1,  0,  0,  0,  0,  1],
+        /*f*/ [0,  0,  1,  0,  0,  0,  1],
+        /*g*/ [0,  0,  0,  0,  1,  1,  0],
+      ])
+    })
   })
 })
--- cs/graphs.ts
+++ cs/graphs.ts
@@ -7,6 +7,73 @@ export class GraphNode<T> {
   ) {}
 }
 
+export class AdjacencyNode<T> {
+  constructor(
+    public value: T,
+  ) {}
+}
+
+/**
+ * Adjacency Matrix Graph
+ *
+ * - Stores relationships between vertices using a matrix (2D array)
+ *   - Each element in 2D array represents whether two vertices are connected
+ *         node1 node2 node3
+ *   node1   0     1     0
+ *   node2   1     0     1
+ *   node3   0     1     0
+ *   (node1 connects to node2; node2 connects to node3)
+ * - Pros:
+ *   - Supports matrix mathematical operations
+ *   - More space and time complexity than Adjacency List
+ */
+export class AdjacencyMatrixGraph<T> {
+  private nodeList: AdjacencyNode<T>[] = []
+  private edgeLists: number[][] = []
+
+  addNode(node: AdjacencyNode<T>): void {
+    this.nodeList.push(node)
+
+    this.edgeLists.forEach((edgeList) => {
+      edgeList.push(0)
+    })
+
+    const newEdgeList: number[] = []
+    this.nodeList.forEach(() => newEdgeList.push(0))
+    this.edgeLists.push(newEdgeList)
+  }
+
+  addEdge(node1: AdjacencyNode<T>, node2: AdjacencyNode<T>): void {
+    const index1 = this.nodeList.indexOf(node1)
+    const index2 = this.nodeList.indexOf(node2)
+
+    this.edgeLists[index1][index2] = 1
+    this.edgeLists[index2][index1] = 1
+  }
+
+  removeNode(node: AdjacencyNode<T>): void {
+    const nodeIndex = this.nodeList.indexOf(node)
+
+    this.nodeList.splice(nodeIndex, 1)
+    this.edgeLists.splice(nodeIndex, 1)
+    this.edgeLists.forEach((edges) => {
+      edges.splice(nodeIndex, 1)
+    })
+  }
+
+  removeEdge(node1: AdjacencyNode<T>, node2: AdjacencyNode<T>): void {
+    const index1 = this.nodeList.indexOf(node1)
+    const index2 = this.nodeList.indexOf(node2)
+
+    this.edgeLists[index1][index2] = 0
+    this.edgeLists[index2][index1] = 0
+  }
+
+  toArray(): number[][] {
+    return this.edgeLists
+  }
+}
+
 export const traverse = <T>(
   startNode: GraphNode<T>,
   operationFn: (value: T) => void,

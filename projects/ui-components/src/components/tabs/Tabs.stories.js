273f702deebde654136d906051080069bc78013e
Will Hastings <will@objectdotcreate.net>
2021-01-23 12:41:06 -0800

    ui-components: Add Tabs components

---
+++ projects/ui-components/src/components/tabs/Tabs.stories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Tabs from './Tabs';
+import Tab from './Tab';
+import TabsList from './TabsList';
+import TabPanel from './TabPanel';
+
+export default {
+  title: 'Components/Tabs',
+  component: Tabs,
+};
+
+export const Basic = () => {
+  return (
+    <Tabs name="Basic Tabs">
+      <TabsList>
+        <Tab>One</Tab>
+        <Tab>Two</Tab>
+        <Tab>Three</Tab>
+      </TabsList>
+      <TabPanel>
+        This is the first tab
+      </TabPanel>
+      <TabPanel>
+        This is the second tab
+      </TabPanel>
+      <TabPanel>
+        This is the third tab
+      </TabPanel>
+    </Tabs>
+  );
+};

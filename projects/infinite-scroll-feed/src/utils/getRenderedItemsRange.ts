1fc78b0198fac2ec9b0fb4567fa2876ad0bddfb9
Will Hastings <will@objectdotcreate.net>
2021-03-21 15:32:53 -0700

    WIP

--- projects/infinite-scroll-feed/src/utils/getRenderedItemsRange.ts
+++ projects/infinite-scroll-feed/src/utils/getRenderedItemsRange.ts
@@ -2,12 +2,70 @@ import { FeedItemInfo, RenderedRange } from '../types';
 
 const NUM_OVERSCAN_ITEMS = 2;
 
+function isInViewport(
+  feedItemInfo: FeedItemInfo,
+  viewportTopPosition: number,
+  viewportBottomPosition: number,
+): boolean {
+  const itemStartPosition = feedItemInfo.position;
+  const itemEndPosition = feedItemInfo.position + feedItemInfo.height;
+
+  return (
+    (itemStartPosition >= viewportTopPosition &&
+      itemStartPosition < viewportBottomPosition) ||
+    (itemEndPosition > viewportTopPosition &&
+      itemEndPosition <= viewportBottomPosition) ||
+    (viewportTopPosition >= itemStartPosition &&
+      viewportBottomPosition <= itemEndPosition)
+  );
+}
+
+function findItemInViewport(
+  feedItemsInfo: FeedItemInfo[],
+  viewportTopPosition: number,
+  viewportBottomPosition: number,
+  searchStart = 0,
+  searchEnd = feedItemsInfo.length - 1,
+): number {
+  const middleIndex = Math.floor((searchEnd + searchStart) / 2);
+  const middleItem = feedItemsInfo[middleIndex];
+
+  if (isInViewport(middleItem, viewportTopPosition, viewportBottomPosition)) {
+    return middleIndex;
+  }
+
+  if (viewportBottomPosition <= middleItem.position) {
+    return findItemInViewport(
+      feedItemsInfo,
+      viewportTopPosition,
+      viewportBottomPosition,
+      searchStart,
+      middleIndex - 1,
+    );
+  }
+
+  return findItemInViewport(
+    feedItemsInfo,
+    viewportTopPosition,
+    viewportBottomPosition,
+    middleIndex + 1,
+    searchEnd,
+  );
+}
+
 export default function getRenderedItemsRange(
   feedItemsInfo: FeedItemInfo[],
 ): RenderedRange {
   const viewportTopPosition = window.scrollY;
   const viewportBottomPosition = window.scrollY + window.innerHeight;
   const feedItemsInViewport: number[] = [];
+
+  const index = findItemInViewport(
+    feedItemsInfo,
+    viewportTopPosition,
+    viewportBottomPosition,
+  );
+  console.log('ITEM ', index, feedItemsInfo[index]);
 
   for (let i = 0; i < feedItemsInfo.length; i++) {
     const itemInfo = feedItemsInfo[i];

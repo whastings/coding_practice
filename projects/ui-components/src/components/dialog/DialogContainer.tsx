05df313674dc47e3430b64c3878644de7a820ce8
Will Hastings <will@objectdotcreate.net>
2021-07-31 14:27:21 -0700

    dialog: Close on escape press

--- projects/ui-components/src/components/dialog/DialogContainer.tsx
+++ projects/ui-components/src/components/dialog/DialogContainer.tsx
@@ -5,6 +5,7 @@ import FocusContainer from '../../utils/FocusContainer';
 import KeyboardEventsProvider from '../../utils/keyboardEvents/KeyboardEventsProvider';
 import styles from './DialogContainer.module.css';
 import UnstyledButton from '../buttons/UnstyledButton';
+import useKeyboardEvent from '../../utils/keyboardEvents/useKeyboardEvent';
 
 interface Props {
   children: React.ReactChild;
@@ -12,34 +13,42 @@ interface Props {
   title: string;
 }
 
-function DialogContainer({ children, onCloseClick, title }: Props) {
+function DialogContainerInner({ children, onCloseClick, title }: Props) {
+  useKeyboardEvent({
+    callback: onCloseClick,
+    key: 'Escape',
+  });
+
   return (
     <div className={styles.card}>
-      <KeyboardEventsProvider>
-        <FocusContainer>
-          {({ initialFocusRef }) => (
-            <>
-              <div className={styles.header}>
-                <h2
-                  className={styles.heading}
-                  ref={initialFocusRef}
-                  tabIndex={-1}
-                >
-                  {title}
-                </h2>
-                <UnstyledButton
-                  aria-label="Close dialog"
-                  onClick={onCloseClick}
-                >
-                  <XIcon className={styles.xIcon} />
-                </UnstyledButton>
-              </div>
-              {children}
-            </>
-          )}
-        </FocusContainer>
-      </KeyboardEventsProvider>
+      <FocusContainer>
+        {({ initialFocusRef }) => (
+          <>
+            <div className={styles.header}>
+              <h2
+                className={styles.heading}
+                ref={initialFocusRef}
+                tabIndex={-1}
+              >
+                {title}
+              </h2>
+              <UnstyledButton aria-label="Close dialog" onClick={onCloseClick}>
+                <XIcon className={styles.xIcon} />
+              </UnstyledButton>
+            </div>
+            {children}
+          </>
+        )}
+      </FocusContainer>
     </div>
+  );
+}
+
+function DialogContainer(props: Props) {
+  return (
+    <KeyboardEventsProvider>
+      <DialogContainerInner {...props} />
+    </KeyboardEventsProvider>
   );
 }
 

3359524b1c00aa1b5c389b5ce44af092830e2bb1
Will Hastings <will@objectdotcreate.net>
2022-01-22 15:27:18 -0800

    [rn][tabs] Basic implementation

--- projects/rn_playground/src/components/Tabs/Tabs.stories.tsx
+++ projects/rn_playground/src/components/Tabs/Tabs.stories.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Text } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 
 import CenterView from '../CenterView';
 import Tabs from './Tabs';
 
 storiesOf('Components/Tabs', module).add('Default', () => {
-  return (
-    <CenterView>
-      <Tabs />
-    </CenterView>
-  );
+  function Default() {
+    const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+    const tabs = [
+      { content: () => <Text>Content A</Text>, label: 'Tab A' },
+      { content: () => <Text>Content B</Text>, label: 'Tab B' },
+      { content: () => <Text>Content C</Text>, label: 'Tab C' },
+    ];
+
+    return (
+      <CenterView>
+        <Tabs
+          activeTabIndex={activeTabIndex}
+          onActiveTabChange={setActiveTabIndex}
+          tabs={tabs}
+        />
+      </CenterView>
+    );
+  }
+
+  return <Default />;
 });
--- projects/rn_playground/src/components/Tabs/Tabs.tsx
+++ projects/rn_playground/src/components/Tabs/Tabs.tsx
@@ -1,10 +1,72 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-function Tabs() {
+interface TabConfig {
+  content: () => React.ReactNode;
+  label: string;
+}
+
+interface Props {
+  activeTabIndex: number;
+  onActiveTabChange: (index: number) => void;
+  tabs: TabConfig[];
+}
+
+const styles = StyleSheet.create({
+  root: {
+    width: '100%',
+  },
+  tab: {
+    alignItems: 'center',
+    flexGrow: 1,
+    padding: 8,
+  },
+  tabContent: {
+    paddingBottom: 16,
+    paddingHorizontal: 16,
+  },
+  tabList: {
+    flexDirection: 'row',
+  },
+  tabText: {
+    fontSize: 18,
+  },
+  tabTextActive: {
+    fontWeight: 'bold',
+  },
+});
+
+function Tabs({ activeTabIndex, onActiveTabChange, tabs }: Props) {
+  const activeTab = tabs[activeTabIndex];
+
   return (
-    <View>
-      <Text>TODO</Text>
+    <View style={styles.root}>
+      <View
+        accessible={true}
+        accessibilityRole="tablist"
+        style={styles.tabList}
+      >
+        {tabs.map((tab, i) => {
+          const isActiveTab = tab === activeTab;
+
+          return (
+            <Pressable
+              accessible={true}
+              accessibilityRole="tab"
+              key={tab.label}
+              onPress={() => onActiveTabChange(i)}
+              style={styles.tab}
+            >
+              <Text
+                style={[styles.tabText, isActiveTab && styles.tabTextActive]}
+              >
+                {tab.label}
+              </Text>
+            </Pressable>
+          );
+        })}
+      </View>
+      <View style={styles.tabContent}>{activeTab.content()}</View>
     </View>
   );
 }

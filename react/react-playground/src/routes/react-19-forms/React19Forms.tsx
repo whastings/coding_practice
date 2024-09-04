9bc72a8341af1ae2244cb1e19d3275d7bbe1fa2e
Will Hastings <will@objectdotcreate.net>
2024-09-03 17:01:00 -0700

    react-playground: Create Simple Calculator React 19 form example

--- react/react-playground/src/routes/react-19-forms/React19Forms.tsx
+++ react/react-playground/src/routes/react-19-forms/React19Forms.tsx
@@ -1,7 +1,71 @@
+import { useActionState, useMemo } from "react";
+
+type Operator = "+" | "-" | "*" | "/";
+
+interface CalcInput {
+  operand1: number;
+  operand2: number;
+  operator: Operator;
+}
+
 function React19Forms() {
+  const [state, action] = useActionState<CalcInput | null>((_, formData) => {
+    const operand1 = formData.get("operand1");
+    const operand2 = formData.get("operand2");
+    const operator = formData.get("operator");
+
+    if (operand1 == null || operand2 == null || operator == null) {
+      return null;
+    }
+
+    return {
+      operand1: Number(operand1),
+      operand2: Number(operand2),
+      operator: operator as Operator,
+    };
+  }, null);
+
+  const answer = useMemo(() => {
+    if (state == null) {
+      return null;
+    }
+
+    const { operand1, operand2, operator } = state;
+
+    switch (operator) {
+      case "+":
+        return operand1 + operand2;
+      case "-":
+        return operand1 - operand2;
+      case "*":
+        return operand1 * operand2;
+      case "/":
+        return operand1 / operand2;
+    }
+  }, [state]);
+
   return (
     <main>
       <h1>React 19 Forms</h1>
+
+      <h2>Simple Calculator</h2>
+      <form action={action}>
+        <label>Operand 1</label>
+        <input type="number" name="operand1" />
+        <label>Operand 2</label>
+        <input type="number" name="operand2" />
+        <label>Operator</label>
+        <select name="operator">
+          <option>+</option>
+          <option>-</option>
+          <option>*</option>
+          <option>/</option>
+        </select>
+        <button type="submit">Calculate</button>
+      </form>
+
+      <h3>Result</h3>
+      {answer}
     </main>
   );
 }
--- react/react-playground/src/vite-env.d.ts
+++ react/react-playground/src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+declare namespace React {
+  export function useActionState<State>(
+    action: (
+      state: Awaited<State>,
+      formData: FormData
+    ) => State | Promise<State>,
+    initialState: Awaited<State>,
+    permalink?: string
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ): [State, any];
+}
